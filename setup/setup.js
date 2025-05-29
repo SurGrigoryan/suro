102e23d37f231bee8a8d2f3c669b68e56a8f4e7a
david <david_mamikonyan@epam.com>
2025-05-29 14:16:02 +0400

    first

--- setup/setup.js
+++ setup/setup.js
@@ -20,19 +20,19 @@ mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 //   process.exit();
 // }
 
-// async function loadData() {
-//   try {
-//     await Item.insertMany(patients);
-//     console.log("👍👍👍👍👍👍👍👍 Done!");
-//     process.exit();
-//   } catch (e) {
-//     console.log(
-//       "\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n"
-//     );
-//     console.log(e);
-//     process.exit();
-//   }
-// }
+async function loadData() {
+  try {
+    await Item.insertMany(patients);
+    console.log("👍👍👍👍👍👍👍👍 Done!");
+    process.exit();
+  } catch (e) {
+    console.log(
+      "\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n"
+    );
+    console.log(e);
+    process.exit();
+  }
+}
 
 async function createAdmin() {
   try {
